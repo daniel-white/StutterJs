05a6c2f3042dd14170d01e5212c3d86fb7a7a76a
Daniel White <dwhite@thetrueaplus.com>
2012-08-11 15:47:29 -0400

    added example. added always callback and functional time support

--- src/stutter.js
+++ src/stutter.js
@@ -1,12 +1,16 @@
 ﻿(function (window) {
     'use strict';
-    var Stutter = function (time, completedCallback, completedCallbackContext, data) {
+    var pushCallback = function (collection, callback, context) {
+        collection.push({ callback: callback, context: context });
+    },
+    Stutter = function (time, completedCallback, completedCallbackContext, data) {
         this._state = {
             data: data,
             id: undefined,
             time: time || 1000,
             complete: [],
-            cancel: []
+            cancel: [],
+            always: []
         };
 
         if (completedCallback !== undefined) {
@@ -14,9 +18,6 @@
         }
 
         return this;
-    },
-    pushCallback = function (collection, callback, context) {
-        collection.push({ callback: callback, context: context });
     };
 
     Stutter.prototype.complete = function (callback, context) {
@@ -29,8 +30,14 @@
         return this;
     };
 
+    Stutter.prototype.always = function (callback, context) {
+        pushCallback(this._state.always, callback, context);
+    };
+
     Stutter.prototype.set = function (data) {
         var state = this._state,
+        self = this,
+        time = state.time,
         makeCallbacks = function (callbacks) {
             var i = 0, callback;
             for (i = 0; i < callbacks.length; i++) {
@@ -39,18 +46,25 @@
                 if (typeof data === 'function') {
                     data = data();
                 }
-                callback.callback.apply(callback.context, [data]);
+                callback.callback.apply(callback.context, [data, self]);
             }
         };
+
+        makeCallbacks(state.always);
 
         if (state.id !== undefined) {
             clearTimeout(state.id);
             makeCallbacks(state.cancel);
         }
 
+        if (typeof time === 'function') {
+            time = time(data);
+        }
+
         state.id = setTimeout(function () {
+            state.id = undefined;
             makeCallbacks(state.complete);
-        }, state.time);
+        }, time);
     };
 
     window.Stutter = Stutter;
