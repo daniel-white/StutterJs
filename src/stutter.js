9e1181b6accd72e6fc151ac5c5973caef6ff9f46
Daniel White <dwhite@thetrueaplus.com>
2012-08-11 15:23:24 -0400

    minified and reduced the complexity of the object model

--- src/stutter.js
+++ src/stutter.js
@@ -1,64 +1,56 @@
-﻿(function (window, undefined) {
-    var _items = [],
-        _Item = function (key) {
-            _items[key] = this;
-            return this;
+﻿(function (window) {
+    'use strict';
+    var Stutter = function (time, completedCallback, completedCallbackContext, data) {
+        this._state = {
+            data: data,
+            id: undefined,
+            time: time || 1000,
+            complete: [],
+            cancel: []
         };
 
-    _Item.prototype.timeoutHandle = '';
-    _Item.prototype.timeout = 1000;
-    _Item.prototype.completedCallbacks = [];
-    _Item.prototype.cancelCallbacks = [];
-    _Item.prototype.addCompletedCallback = function (callback, context) {
-        this.completedCallbacks.push({ callback: callback, context: context });
-    };
-    _Item.prototype.addCancelCallback = function (callback, context) {
-        this.cancelCallbacks.push({ callback: callback, context: context });
-    };
-
-    var Stutter = function (key, timeout, completedCallback, completedCallbackContext) {
-        var item = new _Item(key);
-
-        item.timeout = timeout || item.timeout;
-
-        if (completedCallback != undefined) {
-            item.addCompletedCallback(completedCallback, completedCallbackContext);
+        if (completedCallback !== undefined) {
+            this.complete(completedCallback, completedCallbackContext);
         }
 
-        this._key = key;
         return this;
+    },
+    pushCallback = function (collection, callback, context) {
+        collection.push({ callback: callback, context: context });
     };
 
     Stutter.prototype.complete = function (callback, context) {
-        var item = _items[this._key];
-        item.addCompletedCallback(callback, context);
+        pushCallback(this._state.complete, callback, context);
         return this;
     };
 
     Stutter.prototype.cancel = function (callback, context) {
-        var item = _items[this._key];
-        item.addCancelCallback(callback, context);
+        pushCallback(this._state.cancel, callback, context);
         return this;
     };
 
-    Stutter.prototype.set = function () {
-        var item = _items[this._key];
-
-        var makeCallbacks = function (callbacks) {
-            for (var i = 0; i < callbacks.length; i++) {
-                var callback = callbacks[i];
-                callback.callback.apply(callback.context);
+    Stutter.prototype.set = function (data) {
+        var state = this._state,
+        makeCallbacks = function (callbacks) {
+            var i = 0, callback;
+            for (i = 0; i < callbacks.length; i++) {
+                callback = callbacks[i];
+                data = data || state.data;
+                if (typeof data === 'function') {
+                    data = data();
+                }
+                callback.callback.apply(callback.context, [data]);
             }
+        };
+
+        if (state.id !== undefined) {
+            clearTimeout(state.id);
+            makeCallbacks(state.cancel);
         }
 
-        if (item.timeoutHandle !== '') {
-            clearTimeout(item.timeoutHandle);
-            makeCallbacks(item.cancelCallbacks);
-        }
-
-        item.timeoutHandle = setTimeout(function () {
-            makeCallbacks(item.completedCallbacks);
-        }, item.timeout);
+        state.id = setTimeout(function () {
+            makeCallbacks(state.complete);
+        }, state.time);
     };
 
     window.Stutter = Stutter;
